24ccca839f78aafd38b5e715d833df2715563095
Rakshit Pathak <rajatpathak435@gmail.com>
2020-05-29 12:58:52 +0530

    Home page added, with styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import classes from './Home.module.css';
+import svgImage from '../../assets/quizHome.svg';
 
-const Home = (props) => {
-  return <div></div>;
+const Home = () => {
+  return (
+    <div className={classes.Home}>
+      <h1>WeebCheck</h1>
+      <p>Do you like anime? Join the quiz game and see how you perform!</p>
+      <img height="300" width="500" src={svgImage} alt="" />
+      <button className={classes.Quick}>Quick Game</button>
+      <button className={classes.Custom}>Custom</button>
+    </div>
+  );
 };
 
 export default Home;
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import classes from './Layout.module.css';
+import Home from '../components/Home/Home';
 
+const API =
+  'https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple';
 export default class Layout extends Component {
+  state = {
+    questions: null,
+  };
+
+  async componentDidMount() {
+    const res = await fetch(API);
+    const data = await res.json();
+    const questionArr = [];
+    data.results.forEach((ques) => {
+      const { incorrect_answers, correct_answer, question } = ques;
+      const options = [...incorrect_answers, correct_answer];
+      const obj = {
+        question: question,
+        options,
+        correct: correct_answer,
+      };
+      questionArr.push(obj);
+    });
+    this.setState({ questions: questionArr });
+  }
   render() {
-    return <div className={classes.Layout}></div>;
+    return (
+      <div className={classes.Layout}>
+        <Home />
+      </div>
+    );
   }
 }
